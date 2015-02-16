fb1c4d8fcfa68627ffb02fc80c6107534ce8b764
Andrew <aribicki@gmail.com>
2015-02-16 13:09:37 -0500

    pushing to github

---
+++ MusicDriver.java
@@ -0,0 +1,8 @@
+public class MusicDriver {
+  public static void main(String[] args) {
+	Music myMusic = new Music();
+	System.out.println(myMusic.toString());
+	Music anotherMusic = new Music("Back in Black", "AC/DC", "Metal", "CD");        System.out.println(anotherMusic.toString());
+}
+  }
+
